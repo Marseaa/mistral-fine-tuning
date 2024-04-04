6813a754f8d89444ac11775ce3063d4bfb5f7b73
Marseaa <marcelapasquadealmeida@gmail.com>
2024-04-04 11:01:10 -0300

    correcao de erros

--- inferencia/app.py
+++ inferencia/app.py
@@ -1,3 +1,49 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+# device = "cuda"
+
+def gerar_texto(prompt):
+    # carregar modelo salvo no huggingface para realizar a inferencia
+
+    # carrega modelo
+    model = AutoModelForCausalLM.from_pretrained("Marseaa/Mistral-7B-Summarization", torch_dtype="auto", trust_remote_code=True)
+    
+    # carrega tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("Marseaa/Mistral-7B-Summarization", trust_remote_code=True)
+
+    inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=True)
+    outputs = model.generate(**inputs, max_length=24576)
+    texto_gerado = tokenizer.batch_decode(outputs)[0]
+
+    return texto_gerado
+
+if __name__ == "__main__":
+
+    while True:
+        print("\n\n----GERADOR DE TEXTO PHI-1.5----")
+        print("1 - Gerar texto\n2 - Sair")
+        op = input("Selecione a opcao: ")
+
+        if op == "1":
+
+            prompt = input("Insira o texto inicial: ")
+            texto_gerado = gerar_texto(prompt)
+            print("Texto Gerado:")
+            print(texto_gerado)
+
+        elif op == "2":
+            break
+
+        else:
+            print("Opção inválida. Por favor, selecione novamente.")
+
+
+
+"""
+Modelo com erro:
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 device = "cuda"
@@ -11,16 +57,7 @@ tokenizer = AutoTokenizer.from_pretrained("Marseaa/Mistral-7B-Summarization")
 model = AutoModelForCausalLM.from_pretrained("Marseaa/Mistral-7B-Summarization").to(device)
 
 inputs = tokenizer(
-    """Summarize the following story in my style
-
-### Story
-Chapter 1: The Whispering Peaks
-Dr. Elena Vasquez peered out of the small airplane window, her eyes tracing the outline of the jagged mountains that loomed below. The Andean sun bathed the landscape in a harsh, unforgiving light, revealing a world untouched and unexplored. She adjusted her glasses, her heart racing with the anticipation of setting foot on a land that had, until now, been nothing more than a myth.
-
-For years, Elena had scoured ancient texts and interviewed locals, piecing together tales of the "Whispering Peaks," a remote mountain range shrouded in mystery and rumored to be the home of undiscovered flora and fauna. As a botanist with a penchant for adventure, she was drawn to the unknown, and the possibility of uncovering new species was a call she couldn't resist.
-
-
-### Summary
+    """
 
 """,
     return_tensors = "pt",
@@ -40,3 +77,4 @@ generated_text = tokenizer.batch_decode(outputs, skip_special_tokens = True)
 print("\n\nTEXTO GERADO: \n")
 print(generated_text)
 
+"""
