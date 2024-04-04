8348eb22b7fa3ad19a735f7ad3e7c1932e123bd9
Marseaa <marcelapasquadealmeida@gmail.com>
2024-04-04 12:11:17 -0300

    atualizando

--- inferencia/app.py
+++ inferencia/app.py
@@ -1,4 +1,5 @@
 import torch
+import os
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
@@ -14,7 +15,7 @@ def gerar_texto(prompt):
     tokenizer = AutoTokenizer.from_pretrained("Marseaa/Mistral-7B-Summarization", trust_remote_code=True)
 
     inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=True)
-    outputs = model.generate(**inputs, max_length=24576)
+    outputs = model.generate(**inputs, max_length=300) #num max de caracteres -> pode aumentar
     texto_gerado = tokenizer.batch_decode(outputs)[0]
 
     return texto_gerado
@@ -22,18 +23,29 @@ def gerar_texto(prompt):
 if __name__ == "__main__":
 
     while True:
+        os.system("cls")
         print("\n\n----GERADOR DE TEXTO PHI-1.5----")
         print("1 - Gerar texto\n2 - Sair")
         op = input("Selecione a opcao: ")
 
         if op == "1":
-
+            os.system("cls")
+            print("\n\n----GERADOR DE TEXTO PHI-1.5----")
             prompt = input("Insira o texto inicial: ")
+
             texto_gerado = gerar_texto(prompt)
-            print("Texto Gerado:")
+
+            os.system("cls")
+            print("\n\n----GERADOR DE TEXTO PHI-1.5----")
+
+            print("\nTexto Gerado:\n")
             print(texto_gerado)
+            print("Pressione Enter para continuar...")
+            input()
 
         elif op == "2":
+            os.system("cls")
+            print("\n\nSaindo...\n\n")
             break
 
         else:
